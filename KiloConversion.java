9d78e3ffe81c18eabb39b494361dffe0bfb6d607
Rabins Sharma Lamichhane <rabinsrabins@gmail.com>
2024-09-10 08:57:14 +1000

    KiloConversion: added constructor

--- KiloConversion.java
+++ KiloConversion.java
@@ -13,6 +13,7 @@ public class KiloConversion extends JFrame {
     private JLabel messageLabel; // Message to show to the user for input label
     private JTextField inputText; // To get user input (Kilometer value)
     private JButton convert; // Button for Kilometer to Miles converter
+    private JLabel successMessage; // Showing message after conversion
 
     public final int WINDOW_WIDTH = 400; // Width of the window
     public final int WINDOW_HEIGHT = 100; // Height of the window
@@ -26,16 +27,90 @@ public class KiloConversion extends JFrame {
 
     public KiloConversion() {
 
-        setTitle("Kilometers to Miles Conversion"); // Title of the window
+        // Set the title.
+        setTitle("Kilometers to Miles Converter");
 
-        // Set size of the windows
-
+        // Set the size of the window.
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // Specify an action for the close button.
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Build the panel and add it to the frame.
+        buildPanel();
+
+        // Add the panel to the frame's content pane.
+        add(holdingPanel);
 
         setVisible(true);
 
+    }
+
+    /**
+     * The buildPanel method adds a label, text field, and
+     * and three buttons to a panel.
+     */
+
+    public void buildPanel() {
+
+        messageLabel = new JLabel("Enter number in kilometers: ");
+        inputText = new JTextField(10);
+        successMessage = new JLabel("");
+
+        convert = new JButton("Convert");
+
+        // Add action listner to the Convert button
+
+        convert.addActionListener(new ConvertButtonListner());
+
+        // Create a panel and the components to it.
+
+        holdingPanel = new JPanel();
+
+        holdingPanel.add(messageLabel);
+        holdingPanel.add(inputText);
+        holdingPanel.add(convert);
+        holdingPanel.add(successMessage);
+
+    }
+
+    /**
+     * Private inner class that handles the event when
+     * the user clicks one of the Convert Button.
+     */
+
+    private class ConvertButtonListner implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            String input; // To hold the user's input
+
+            // Get the kilometers entered by the user
+            input = inputText.getText();
+            double results = 0.00; // The units we are converting
+
+            try {
+
+                results = Double.parseDouble(input) * 0.62137; // KM to Miles conversion formula
+                // Display the conversion.
+
+            }
+
+            catch (NumberFormatException ex) {
+                successMessage.setText("Please enter a valid number");
+
+            }
+        }
+    }
+
+    /**
+     * The main method creates an instance of the
+     * KiloConversion class, displaying its window.
+     */
+
+    public static void main(String[] args) {
+
+        new KiloConversion();
     }
 
 }
